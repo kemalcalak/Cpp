4bcb9e9a9ddbaf05a2318b96084c3847266e6348
Ali Kemal Çalak <kemalcalak@gmail.com>
2023-10-10 10:15:23 +0300

    Tarih Sınıfı Ödev

---
+++ historyClass/historyClass/historyClass.cpp
@@ -0,0 +1,25 @@
+﻿#include "date.h"
+
+
+int Main() {
+	Date mainDate(02, 01, 2020);
+	cout << "Day:" << mainDate.getDay() << endl;
+	cout << "Day:" << mainDate.getMonth() << endl;
+	cout << "Day:" << mainDate.getYear() << endl;
+	mainDate.displayDate();
+
+	Date mainDate2(01, 01, 2020);
+	mainDate2.displayDate();
+
+
+
+	mainDate.compareDate(mainDate2);
+
+	cout << "------------------------------" << endl;
+	for (int i = 0; i < 30; i++) {
+		mainDate2.increaseDay();
+		mainDate2.displayDate();
+	}
+}
+
+
