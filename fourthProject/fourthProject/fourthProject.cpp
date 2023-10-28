b278c752076b678aa1e951fd8871bc2adffbcbd3
Ali Kemal Çalak <kemalcalak@gmail.com>
2023-10-28 16:55:28 +0300

    Example related to inheritance

---
+++ fourthProject/fourthProject/fourthProject.cpp
@@ -0,0 +1,20 @@
+﻿#include "person.h"
+#include <time.h>
+
+int main()
+{
+
+    srand(time(NULL));
+    Student studentArray[] = { {"Ogun","Birinci",1000},{"Duygu","Keydal",1001},{"Eda","Deniz",1002,},{"Burak","Ikinci",1003} };
+    Teacher teacher1("Oguz", "Birinci", "Software");
+
+    for (int i = 0; i < 4; i++) {
+        studentArray[i].setStudentGrade(rand() % 100 + 1);
+        studentArray[i].print();
+    }
+
+    teacher1.passFailStatus(studentArray);
+    teacher1.printStudentInformation(studentArray);
+
+    return 0;
+}
