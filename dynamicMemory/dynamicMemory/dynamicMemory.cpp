37982ca9cabddac0b00d599d5184116b3aff98d5
Ali Kemal Çalak <kemalcalak@gmail.com>
2023-10-29 10:45:02 +0300

    Dynamic object creation and use of this

--- dynamicMemory/dynamicMemory/dynamicMemory.cpp
+++ dynamicMemory/dynamicMemory/dynamicMemory.cpp
@@ -13,11 +13,59 @@ int main()
 	(*exPtr).y = 4;
 	(*exPtr).print();
 	*/
-
-
+	/*
 	exPtr->x = 6;
 	exPtr->y = 4;
 	exPtr->print();
 
+	int* ptr = new int;
+	Example* ptrExample = new Example;
+
+	ptrExample->x = 5;
+	ptrExample->y = 7;
+	ptrExample->print();
+
+	delete ptrExample;
+	delete ptr;
+	*/
+	/*
+	int* ptrArray = new int[5];
+	Example* ptrExArray = new Example[5];
+	
+	for (int i = 0; i < 5; i++) {
+		
+		//ptrArray[i] = i;
+		//cout << ptrArray[i] << endl;
+		
+		ptrExArray[i].x = i;
+		ptrExArray[i].y = i * i;
+		ptrExArray[i].print();
+	}
+	delete[]ptrExArray;
+	delete[]ptrArray;
+	*/
+	/*
+	int** ptr2DArray = new int* [5];
+	Example** ptrEx2DArray = new Example * [5];
+	for (int i = 0; i < 5; i++) {
+		//ptr2DArray[i] = new int[5];
+		ptrEx2DArray[i] = new Example[5];
+	}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			//ptr2DArray[i][j] = i;
+			ptrEx2DArray[i][j].x = i;
+			ptrEx2DArray[i][j].y = j;
+		}
+	}
+	for (int i = 0; i < 5; i++) {
+		delete[]ptrEx2DArray[i];
+	}
+	delete[]ptrEx2DArray;
+	*/
+
+	Example exObj(5,5), exObj2(4,4);
+	Example exObj3 = exObj.addExample(exObj2);
+	exObj3.print();
 
 }
--- dynamicMemory/dynamicMemory/dynamicMemory.h
+++ dynamicMemory/dynamicMemory/dynamicMemory.h
@@ -7,10 +7,20 @@ public:
 	Example() {
 		cout << "Constructor worked." << endl;
 	}
+	Example(int mainX=0,int mainY=0) {
+		this->x = mainX;
+		this->y = mainY;
+		cout << "Constructor worked." << endl;
+	}
 	~Example() {
 		cout << "Destructor worked." << endl;
 	}
 	void print() {
 		cout << "X Value: " << x << "Y Value:" << y << endl;
+	}
+	Example addExample(Example& oth) {
+		this->x += oth.x;
+		this->y += oth.y;
+		return *this;
 	}
 };
